9715a5f432fc48d7ac86a3537f751c68b9913031
Andreas Coroiu <andreas.coroiu@gmail.com>
2017-03-08 10:06:33 +0100

    Fix dijkstra.

--- experiments/path_planning_planner/dijkstra.m
+++ experiments/path_planning_planner/dijkstra.m
@@ -2,20 +2,24 @@ function [ distances ] = dijkstra( goal, free_matrix, cost_matrix )
 %BFS Performs dijkstra and returns matrix with distance from goal
     
     distances = zeros(size(free_matrix));
+    distances(:) = inf;
+    is_in_queue = zeros(size(free_matrix));
     
     [goal_row, goal_col] = ind2sub(size(free_matrix), goal);
     
     queue = {{}};
     queue{1}.row = goal_row;
     queue{1}.col = goal_col;
-    queue{1}.cost = 1;
+    %queue{1}.cost = 1;
     
     distances(goal_row, goal_col) = 1;
     
     while (size(queue, 2)) 
-        min_i = min_cost(queue);
+        min_i = min_cost(queue, distances);
         pos = queue{min_i};
+        cost = distances(pos.row, pos.col);
         queue(min_i) = [];
+        is_in_queue(pos.row, pos.col) = 0;
         
         % Add adjacency coords to queue.
         for r = -1:1
@@ -27,27 +31,41 @@ function [ distances ] = dijkstra( goal, free_matrix, cost_matrix )
                 new_queue = {};
                 new_queue.row = pos.row + r;
                 new_queue.col = pos.col + c;
-                new_queue.cost = pos.cost + 1 + cost_matrix(new_queue.row, new_queue.col);
+                new_cost = cost + 1 + cost_matrix(new_queue.row, new_queue.col);
                 
-                if (~free_matrix(new_queue.row, new_queue.col) || distances(new_queue.row, new_queue.col))
+                if (~free_matrix(new_queue.row, new_queue.col))
                    continue;
                 end
                 
-                queue{end + 1} = new_queue;
-                distances(new_queue.row, new_queue.col) = new_queue.cost;
+                if (new_cost < distances(new_queue.row, new_queue.col))
+                    index = find_in_queue(new_queue, queue);
+                    distances(new_queue.row, new_queue.col) = new_cost;
+                end
             end
         end
     end
 end
 
-function index = min_cost(queue)
+function index = min_cost(queue, distances)
     index = 1;
     min_node = queue{1};
+    min_cost = distances(min_node.row, min_node.col);
     for i = 2:size(queue, 2)
-        if queue{i}.cost < min_node.cost
+        node = queue{i};
+        cost = distances(node.row, node.col);
+        if cost < min_cost
             index = i;
             min_node = queue{i};
         end
     end
 end
 
+function index = find_in_queue(node, queue)
+    index = 0;
+    for i = 2:size(queue, 2)
+        if queue{i}.row == node.row && queue{i}.col == node.col
+            index = i;
+            return;
+        end
+    end
+end
--- experiments/path_planning_planner/plan.m
+++ experiments/path_planning_planner/plan.m
@@ -12,15 +12,17 @@ grid_size = (size(image.data) * image.resolution) / cell_size;
 
 free_matrix = find_free_cells(image, grid_size);
 
-%obstacle_matrix = distance_to_obstacles(free_matrix);
+obstacle_matrix = distance_to_obstacles(free_matrix);
+%obstacle_matrix = max(max(obstacle_matrix)) - obstacle_matrix;
+%obstacle_matrix = zeros(size(free_matrix));
 goal_cell = find(free_matrix == 1, 1);
 
-distances = bfs(goal_cell, free_matrix);
-%distances = dijkstra(goal_cell, free_matrix, obstacle_matrix);
+%distances = bfs(goal_cell, free_matrix);
+distances = dijkstra(goal_cell, free_matrix, obstacle_matrix);
 distances(free_matrix == 0) = -inf;
 
-% start = find(distances == max(max(distances)), 1);
-start = 8680; % hardcoded position closer to robot start pos in stage.
+start = find(distances == max(max(distances)), 1);
+%start = 8680; % hardcoded position closer to robot start pos in stage.
 path = path_plan(distances, start) - 0.5;
 
 % figure
