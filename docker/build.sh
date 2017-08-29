2997e1cb59c9d252012509ea14e820673230391e
David Gillsjö <davidg@maths.lth.se>
2017-08-29 14:28:10 +0200

    * Image can now run with both intel and nvidia graphics

--- docker/build.sh
+++ docker/build.sh
@@ -3,4 +3,5 @@ docker build\
   -t robotslam\
   --build-arg gid=$(id -g)\
   --build-arg uid=$(id -u)\
+  --build-arg user="$USER"\
   ../.
--- docker/run.sh
+++ docker/run.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
-docker run -it --rm \
-  -v "/tmp/.X11-unix:/tmp/.X11-unix" \
-  -e DISPLAY=$DISPLAY \
-  --name robotslam_c \
-  --hostname robotslam_c \
-  robotslam
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+set -x
+if [[ ${USE_NVIDIA} == 1 ]] ; then
+  DOCKER_CMD="nvidia-docker"
+  DOCKER_OPT=""
+else
+  DOCKER_CMD="docker"
+  DOCKER_OPT="--volume=/dev/dri:/dev/dri:rw \
+              --volume=$XAUTH:$XAUTH:rw \
+              --env=XAUTHORITY=${XAUTH}"
+fi;
+
+"$DOCKER_CMD" run -it ${DOCKER_OPT}\
+        --volume=$XSOCK:$XSOCK:rw \
+        --env="QT_X11_NO_MITSHM=1" \
+        --env="DISPLAY" \
+        -v "host_home:$HOME:rw"\
+    robotslam
+
+set +x
